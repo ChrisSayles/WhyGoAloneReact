d728902df6bace5ec1738df253abf266440e3ebe
Chris Sayles <chrissayles@yahoo.com>
2017-04-11 18:05:31 -0500

    Updated CSS and Component Structure for interest, interest list

--- src/components/chat.js
+++ src/components/chat.js
@@ -4,8 +4,8 @@ class Chat extends Component {
 	render(){
 		return(
 
-			<div className="panel panel-default panel-border col-sm-12">
-  				<h4 className="panel-body chatTitle textStyle">Chat</h4>
+			<div className="panel panel-default panel-border">
+  				<h4 className="panel-body heading-area textStyle">Chat</h4>
   				<div className="panel-body chatBox textStyle">Chat Box</div>
 			</div>
 			)
--- src/components/interest.js
+++ src/components/interest.js
@@ -8,18 +8,39 @@ class Interest extends Component {
   	// 			<div className="panel-body interestBottomTitle textStyle heading-area">Name your Interest</div>
   	// 			<div className="panel-body interestBottomField textStyle">Input Field</div>
 			// </div>
-		<div className="panel panel-default panel-border col-sm-12">
-        <div className="panel-body-search">
-          <h4 className="panel-title text-center interestTopTitle panel-heading textStyle"><strong>Name your interests</strong></h4>
+		// <div className="panel panel-default panel-border col-sm-12">
+  //       <div className="panel-body-search">
+  //         <h4 className="text-center interestTopTitle panel-heading textStyle"><strong>Name your interests</strong></h4>
+  //       </div>
+  //       <div className="panel-body textStyle">
+  //        test
+  //       </div>
+  //     </div>
+<div className="panel panel-default panel-border">
+        <div className="panel-heading heading-area textStyl" id="headingArea">
+                        <h4 className="textStyle"> Name your interest</h4>
         </div>
-        <div className="panel-body textStyle">
-         test
+        <div className="panel-body">
+                        <form id="interestForm">
+                          <div className="form-group bodyBackground ">
+                            <label>
+                              InterestInput:
+                              <input type="text" id="interestInput" class="form-control" placeholder="e.g. Shopping, Karaoke"/>
+                              
+                              <input id="addInterest" type="submit" class="btn btn-primary" value="Add"/>
+
+
+                            </label>
+                          </div>
+                        </form>
+                        <div className="currentCount"></div>
         </div>
-      </div>
-			)
+               
+</div>		
+    	)
 	}
 }
-
+             
 
 
 export default Interest;
